844ec9be60a55e1cc873d9084c0d68e7e83506c3
sagrael1974 <lucky_lukas2001@yahoo.de>
2024-12-12 22:16:19 +0100

    Add Room database dependencies and KSP plugin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
 
 tasks.register("clean", Delete::class) {
